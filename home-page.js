e8cd566bcfa48cc647a5c1078a4c6cab975dc814
SerranoZ <lucas_serrano@id.uff.br>
2022-02-21 11:05:21 -0300

    HOME PAGE PRONTA

--- home-page.js
+++ home-page.js
@@ -1,29 +1,27 @@
 const url = "https://lobinhos.herokuapp.com/wolves"
-const lobosexemplo = document.querySelector(".lobos-exemplo")
+const lobosexemplo = document.querySelector(".lista-lobinhos")
 
 function createPostDiv(wolf){
     const li = document.createElement("li")
     const id = wolf.id
 
     li.className = "li"
-    li.innerHTML = `
-
-            <section>
+    li.innerHTML =    
+    `
+    <img src="${wolf.image_url}">
+        <div class="bloco0">
+            <div class="bloco1">
                 <div>
-                    <div>
-                        <img src="${wolf.image_url}">
-                    </div>
-                    <div>
-                        <div>
-                            <h1>Nome: ${wolf.name}</h1>
-                            <h2>Idade: ${wolf.age} anos</h2>
-                        </div>
-                        <div>
-                            <p>${wolf.description}</p>
-                        </div>
-                    </div>
+                    <h1>Nome: ${wolf.name}</h1>
+                    <h2>Idade: ${wolf.age} anos</h2>
                 </div>
-            </section>`
+                <a href="adotar-lobinho.html">
+                    <input class="adotar" type="button" value="Adotar" target="_self">
+                </a>
+            </div>
+            <p class="texto">${wolf.description}</p>
+        </div>     
+    `
 
 
     lobosexemplo.appendChild(li)
